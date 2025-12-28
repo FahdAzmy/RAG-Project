bafbd5d82dd5ab9bbb293e94553049c4c7f34af9
FahdAzmy <azmyfahd66@gmail.com>
2025-12-28 08:18:51 +0200

    finish uploading file funciton and endpoint

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Depends
 import os 
+from src.helpers.config import get_settings,Settings
 router = APIRouter(
     prefix="/api/v1",
     tags=["api v1"],
 )
 @router.get("/")
-async def welcome_message():
-    app_name=os.getenv("APP_NAME")
-    app_version=os.getenv("APP_version")
-    return {"message": f"Welcome fROM {app_name} {app_version}!"}
+async def welcome_message(app_settings: Settings = Depends(get_settings)):
+   app_name = app_settings.APP_NAME
+   app_version = app_settings.APP_VERSION
+   return {"message": f"Welcome fROM {app_name} {app_version}!"}
